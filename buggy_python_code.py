511e48fa6de9b421d1f2e8ecddf45f9594df26fb
Sebastián Šuťák <xsutak@fi.muni.cz>
2024-04-18 12:56:04 +0200

    white spaces2

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -26,7 +26,7 @@ def print_nametag(format_string, person):
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
     exec(f"import urllib{urllib_version} as urllib", globals())
-    # Fetch and print the requested URL
+    # Fetch and print the requested  URL
  
     try: 
         http = urllib.PoolManager()
